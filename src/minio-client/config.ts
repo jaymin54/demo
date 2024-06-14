ee95fcf8ee1d7dbb6f527ff13ddef860241b3644
jaymin54 <jayminpatel@thingslinker.com>
2024-06-14 12:19:35 +0530

    first commit

---
+++ src/minio-client/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  MINIO_ENDPOINT: 'localhost',
+  MINIO_PORT: 9000,
+  MINIO_ACCESSKEY: 'kdSnkdOhgrRL0tbG6izp',
+  MINIO_SECRETKEY: 'sQxx0mFhprms7Rgl3wZwvAKzQXT4fccIJuf9l6Q9',
+  MINIO_BUCKET: 'b4bucket',
+};
